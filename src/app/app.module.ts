c4e8c54ba4d748d9957d84aa0f2e201b9ffe10e9
Gustavo Diniz Monteiro <gustavo.monteiro@ccc.ufcg.edu.br>
2017-05-03 11:59:09 -0300

    Added search bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SearchBarComponent } from './toolbar/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
