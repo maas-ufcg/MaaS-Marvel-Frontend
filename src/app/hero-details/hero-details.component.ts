92d6e9a168864578fc6c6ef077b4661a73f3da1b
Ygor Santos <ygor.gsan@gmail.com>
2017-05-11 10:06:48 -0300

    created the details card of a hero

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -15,6 +15,8 @@ export class HeroDetailsComponent implements OnInit, OnDestroy {
   id;
   subscription: Subscription;
 
+  private default_resolution = '/standard_fantastic.';
+
   constructor(
     private route: ActivatedRoute,
     private restService: RestService,
@@ -23,16 +25,23 @@ export class HeroDetailsComponent implements OnInit, OnDestroy {
     this.hero = {};
   }
 
+  getHeroImage() {
+    if(this.hero.thumbnail){
+      return this.hero.thumbnail.path + this.default_resolution + this.hero.thumbnail.extension;
+    }
+  }
+
   ngOnInit() {
     this.subscription = this.route.params.subscribe(
       (params: any) => {
         this.id = params['id'];
+        console.log(this.id)
         this.restService.getHero(this.id).then(res => {
           if (!res) {
             this.router.navigate(['notFound'])
           }
-          
           this.hero = res;
+          console.log(this.hero)
         })
       }
     );
